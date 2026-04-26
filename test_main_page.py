1ce927ea85004e39cb8ff177c7e03993ab72385d
werdtr <dollstrash@gmail.com>
2026-04-26 15:06:01 +0300

    MainPage and BasePage classes

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,13 @@
+from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
+
+def go_to_login_page(browser):
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
